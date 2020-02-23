c70759b900503ce0b399322f341ba7bda139a323
Yash Jain <46073057+yashjain092000@users.noreply.github.com>
2020-02-22 23:45:56 -0500

    add initials and switch case stmt.

--- src/Main/Menu.java
+++ src/Main/Menu.java
@@ -1,4 +1,27 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Menu {
+    public static void main(String[] args) {
+        int response;System.out.println("Welcome to Yash's Contact List App\n" +
+                "        Press 1 to add a new contact\n" +
+                "        Press 2 to view all contacts\n" +
+                "        Press 3 to search for a contact\n" +
+                "        Press 4 to delete a contact\n" +
+                "        Press 5 to exit program");
+        Scanner sc = new Scanner(System.in);
+        response = sc.nextInt();
+        switch (response) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+
+
+        }
+
+
+    }
 }
